a03a400225ec264c1b177c3c2c7d1235114f2c4c
pspanayotov <mihail_workbuz@abv.bg>
2014-06-25 20:01:57 +0300

    import registration

--- host/Version1/urls.py
+++ host/Version1/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
-from django.contrib.auth import views as auth_views
+import registration
 import os
 admin.autodiscover()
 def fromRelativePath(*relativeComponents):
