eba31ac0bfa497a132b7a2aee8112b2072102ea4
pspanayotov <mihail_workbuz@abv.bg>
2014-07-01 11:48:57 +0300

    forms.Form

--- host/parkingclient/forms.py
+++ host/parkingclient/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.forms import ModelForm
 from parkingclient.models import RegularUser
 
-class RegistrationForm(ModelForm):
+class RegistrationForm(forms.Form):
     username = forms.CharField()    email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput(render_value=False))
     password1 = forms.CharField(widget=forms.PasswordInput(render_value=False))        
