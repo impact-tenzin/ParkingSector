18c5cba9b26109f3ffbb95896cec19dad6d4ca5a
pspanayotov <mihail_workbuz@abv.bg>
2014-08-03 14:42:56 +0300

    contact_me.js

--- host/static/js/contact_me.js
+++ host/static/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././phpCF/contact_me.php",
+                url: "/static/phpCF/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
